8cc86510feb2754c7a5e3a516d3ef91f8de7f28e
jack <e.kobets@finik.pro>
2018-03-13 01:04:43 +0200

    Execute terminal commands from Java and print commans output in the console.

--- src/main/java/com/gmail/jackkobec/java/core/ExecuteTerminalCommandsFromJava.java
+++ src/main/java/com/gmail/jackkobec/java/core/ExecuteTerminalCommandsFromJava.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 /**
  * @Author Jack <jackkobec>
  */
-public class executeTerminalCommandsFromJava {
+public class ExecuteTerminalCommandsFromJava {
 
     public static void main(String[] args) throws IOException {
         // Gets runtime
